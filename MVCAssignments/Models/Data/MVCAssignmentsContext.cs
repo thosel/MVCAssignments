0af300c4e51d013c5f6c5f82b01f46a2813a1406
Thomas Selin <selin.reso@gmail.com>
2022-02-14 21:53:32 +0100

    Add db context

---
+++ MVCAssignments/Models/Data/MVCAssignmentsContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCAssignments.Models.Data
+{
+    public class MVCAssignmentsContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
