dd5f1b4fea98813e80a7272447219361bb1ac520
Thomas Selin <selin.reso@gmail.com>
2022-02-21 16:54:01 +0100

    Add ApplicationUser

---
+++ MVCAssignments/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCAssignments.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
